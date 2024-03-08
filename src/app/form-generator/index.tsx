47755e07e1dc264c8c006753019970474c89162c
Rishav Sharma <rishavtosharma@gmail.com>
2024-03-08 23:25:34 +0530

    ci: create FormGenerator component and added it to home page

---
+++ src/app/form-generator/index.tsx
@@ -0,0 +1,47 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import {
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogFooter,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+} from "@/components/ui/dialog";
+import { Textarea } from "@/components/ui/textarea";
+import { useState } from "react";
+
+type Props = {};
+const FormGenerator = (props: Props) => {
+    const [isDialogueOpen, setIsDialogueOpen] = useState(false);
+
+    const onSubmit = () => {
+        // e.preventDefault();
+        setIsDialogueOpen(true);
+    };
+    return (
+        <Dialog open={isDialogueOpen} onOpenChange={setIsDialogueOpen}>
+            <Button onClick={onSubmit}>Create Form</Button>
+            <DialogContent className='sm:max-w-[425px]'>
+                <DialogHeader>
+                    <DialogTitle>Create a new form</DialogTitle>
+                    <form>
+                        <div className='grid gap-4 py-4'>
+                            <Textarea
+                                id='description'
+                                name='description'
+                                required
+                                placeholder="How do you want your form to look like? What's it about? What are the questions you want to ask? and so on..."
+                            />
+                        </div>
+                    </form>
+                </DialogHeader>
+                <DialogFooter>
+                    <Button variant='link'> Create Manually</Button>
+                </DialogFooter>
+            </DialogContent>
+        </Dialog>
+    );
+};
+export default FormGenerator;
