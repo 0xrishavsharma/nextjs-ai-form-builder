fecdfc0516d2a501bc475a5822c4f3648a2cd85f
Rishav Sharma <rishavtosharma@gmail.com>
2024-03-17 20:23:29 +0530

    ci: added header component with signin and signout functionality

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,15 @@
-"use client";
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
 import { useState } from "react";
 import FormGenerator from "./form-generator";
+import Header from "@/components/ui/header";
 export default function Home() {
-    const [isDialogueOpen, setIsDialogueOpen] = useState(false);
     return (
-        <main className='flex min-h-screen flex-col items-center justify-between p-24'>
-            <FormGenerator />
-        </main>
+        <>
+            <Header />
+            <main className='flex min-h-screen flex-col items-center justify-between p-24'>
+                <FormGenerator />
+            </main>
+        </>
     );
 }
--- src/auth.ts
+++ src/auth.ts
@@ -1,15 +1,18 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
-export const authOptions = {
-    // Configure one or more authentication providers
+const authOptions = {
     providers: [
         GoogleProvider({
-            clientId: process.env.GOOGLE_CLIENT_ID,
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+            clientId: process.env.GOOGLE_CLIENT_ID ?? "",
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? "",
         }),
-        // ...add more providers here
     ],
 };
 
-export const { handlers, auth, signin, signout } = NextAuth(authOptions);
+export const {
+    handlers: { GET, POST },
+    auth,
+    signIn,
+    signOut,
+} = NextAuth(authOptions);
