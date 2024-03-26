771570213212d2db18d0451712d90f78ebbb759c
Rishav Sharma <rishavtosharma@gmail.com>
2024-03-26 23:11:03 +0530

    ci: completed authentication and ability to generate forms

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,13 +3,14 @@ import { Button } from "@/components/ui/button";
 import { useState } from "react";
 import FormGenerator from "./form-generator";
 import Header from "@/components/ui/header";
+import { SessionProvider } from "next-auth/react";
 export default function Home() {
   return (
-    <>
+    <SessionProvider>
       <Header />
       <main className="flex min-h-screen flex-col items-center justify-between p-24">
         <FormGenerator />
       </main>
-    </>
+    </SessionProvider>
   );
 }
